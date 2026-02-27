b1155a03eb9de16c351d53cfab1bf7c395a9acb3
Jan Mederly <jmederly@evolveum.com>
2026-02-27 16:06:04 +0100

    Fix a bug in jobs.py that cause uploadDocumentation to fail

--- src/common/jobs.py
+++ src/common/jobs.py
@@ -220,7 +220,11 @@ async def schedule_coroutine_job(
                     result_dict = {"value": repr(result)}
 
             # TODO: implement caching also for codegen, move scraper implementation here
-            if "codegen" not in job_type and "scrape" not in job_type and input_payload["usePreviousSessionData"]:
+            if (
+                "codegen" not in job_type
+                and "scrape" not in job_type
+                and input_payload.get("usePreviousSessionData", False)
+            ):
                 logger.info(
                     "[%s] Job %s (session %s): use_previous_session_data is True, checking for previous job output",
                     job_type,
